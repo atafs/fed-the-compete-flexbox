261bad9fbe8346694fe1ef09f2d98d55dcbf5dd2
atafs <americoviagens@gmail.com>
2017-08-30 02:36:13 +0100

    APP: parallax

--- js/animation.js
+++ js/animation.js
@@ -5,10 +5,13 @@ $(document).ready(function() {
     var waypoint = new Waypoint({
       element: document.getElementsByClassName('js--wp-1'),
       handler: function(direction) {
-         $('.js--wp-1').addClass('animated fadeInUp'); 
-         $('.js--wp-2').addClass('animated fadeInUp');  
+         $('.js--wp-1').addClass('animated fadeInUp');
+         $('.js--wp-2').addClass('animated fadeInUp');
          $('.js--wp-3').addClass('animated fadeInUp');
-         $('.js--wp-4').addClass('animated fadeInUp');  
+         $('.js--wp-4').addClass('animated fadeInUp');
       }
     })
+
+    /* Parallax */
+    $('#main').stellar();
 });
